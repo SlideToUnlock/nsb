891a50769c7e1af7ccf776660c5ab7a75e6cc76e
LiuSandy <nsiliushuang@gmail.com>
2018-01-19 17:24:59 +0800

    解决跨域请求

--- src/main/java/com/nsb/NsbApplication.java
+++ src/main/java/com/nsb/NsbApplication.java
@@ -2,9 +2,13 @@ package com.nsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.nsb.*", "com.nsb.config.*"})
@@ -12,5 +16,23 @@ public class NsbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NsbApplication.class, args);
+	}
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		return corsConfiguration;
+	}
+
+	/**
+	 * 跨域过滤器
+	 * @return
+	 */
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig()); // 4
+		return new CorsFilter(source);
 	}
 }
